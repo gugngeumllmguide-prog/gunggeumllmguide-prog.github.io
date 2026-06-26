e0c145531a642ee8ff82c37948b67818e059f4ec
gunggeum <gugngeumllmguide@gmail.com>
2026-06-26 15:44:40 +0900

    .

--- gunggeumllmguide-prog/file/web_.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, redirect
-
-app = Flask(__name__)
-todos = []
-
-@app.route('/')
-def index():
-    return render_template('index.html', todos=todos)
-
